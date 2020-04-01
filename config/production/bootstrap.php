e203ec56641c8cf3b8ec50510852a052c55b177a
AnhMH <anh.mh@moziasoft.com>
2020-04-01 13:23:36 +0700

    Modify production config

--- config/production/bootstrap.php
+++ config/production/bootstrap.php
@@ -7,10 +7,10 @@ use Cake\Core\Configure;
 
 define('USE_SUB_DIRECTORY', '');
 
-Configure::write('API.Host', 'http://api.lyonabeauty.com/public/');
+Configure::write('API.Host', 'http://api.tusachehon.com/public/');
 Configure::write('Config.HTTPS', false);
 
 Configure::write('Config.CKeditor', array(
-    'basel_dir'=>'/home/lyonabea/img.lyonabeauty.com/',
-    'basel_url'=>'https://img.lyonabeauty.com/'
+    'basel_dir'=>'/home/lyonabea/img.tusachehon.com/',
+    'basel_url'=>'https://img.tusachehon.com/'
 ));
